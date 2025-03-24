ec118b3e7806234896344a84152b43ade0e74ff1
Alekkzsx <alex.soares.gabriel111@gmail.com>
2025-03-24 17:10:40 -0300

    Update Tela 1 - Login.py

--- Tela 1 - Login.py
+++ Tela 1 - Login.py
@@ -1,2 +1,12 @@
-print("="*70)
+#Boas vindas ao usuário
+print("="*48)
 print("\tBem Vindo - Faça Seu Login")
+print("="*48)
+
+
+#Entrada de nome de Usuário e Senha
+NomeDeUsuario = input("\t\tNome de Usuário: ")
+
+#Repetição para caso usuário não cadastrado
+while NomeDeUsuario != 
+SenhaDoUsuario = input("\t\tSenha: ")
