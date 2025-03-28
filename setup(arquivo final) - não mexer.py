569a14d400ff2917a26548b073a19ce2de555cc0
Alekkzsx <alex.soares.gabriel111@gmail.com>
2025-03-28 09:24:01 -0300

    Update setup(arquivo final) - não mexer.py

--- setup(arquivo final) - não mexer.py
+++ setup(arquivo final) - não mexer.py
@@ -4,10 +4,8 @@ import tela3_tela_de_trabalho
 def main():
     usuario_logado = tela1_login.executar_fluxo_login()
     
-    # Correção 1: Verificação correta do retorno
-    if usuario_logado:  # Remove a comparação com "True"
-        # Correção 2: Usar a variável correta
-        tela3_tela_de_trabalho.main(usuario_logado)  # "usuario_logado" em vez de "usuario_correto"
+    if usuario_logado:
+        tela3_tela_de_trabalho.main(usuario_logado)
 
 if __name__ == "__main__":
     main()
