cd1e8014ab7c300fc6c5781dc740276b2d737302
Alekkzsx <alex.soares.gabriel111@gmail.com>
2025-04-01 07:54:49 -0300

    Create setup(arquivo final) - não mexer.py

---
+++ setup(arquivo final) - não mexer.py
@@ -0,0 +1,11 @@
+import tela1_login
+import tela3_tela_de_trabalho
+
+def main():
+    usuario_logado = tela1_login.executar_fluxo_login()
+    
+    if usuario_logado:
+        tela3_tela_de_trabalho.main(usuario_logado)
+
+if __name__ == "__main__":
+    main()
