c24425d086a2f6f55066a9ff294cad7084720d7d
Alekkzsx <alex.soares.gabriel111@gmail.com>
2025-04-11 09:27:56 -0300

    Update tela8_calender.py

--- tela8_calender.py
+++ tela8_calender.py
@@ -1,1 +1,12 @@
 #calendário
+"""
+mostrar literalmente o calendário do mês atual e mostrar os dias do mês
+e cada dia deve mostrar em vermelho dias que não houveram registro
+e em verde os dias que houveram registro.
+e caso os registros estejam em mensal, contar verde o mês inteiro
+caso seja diario, marcar em verde só aquele dia
+em caso de anual, mostrar apenas uma mensagem de que o registro é anual, conta para aquele ano, algo assim : "No ano XXXX, voce colocou um registro anual"
+
+Logo abaixo do mes deve haver uma tabela com os dias de registro, respeitando o periodo no qual o registrado (diario,mensal,anual)
+E o sistema deve pedir ao usuario para escolher que mês, ou ano ele quer ver o calendário, e mostrar o calendário do mês ou ano escolhido.
+"""
